3712e5bf7150257b95f57e0d2a255ba2519e2db3
Hans Svensson <hans.svensson@quviq.com>
2017-12-21 12:04:03 +0100

    Minor fixes

--- apps/aeoracle/src/aeo_subscription.erl
+++ apps/aeoracle/src/aeo_subscription.erl
@@ -20,8 +20,8 @@ notify_on_new_transactions([]) ->
 notify_on_new_transactions([SignedTx | Rest]) ->
     Tx = aec_tx_sign:data(SignedTx),
     case aec_tx_dispatcher:handler(Tx) of
-        aeo_query_tx    -> notify_query_tx(Tx);
-        aeo_response_tx -> notify_response_tx(Tx);
+        aeo_query_tx    -> maybe_notify_query_tx(Tx);
+        aeo_response_tx -> maybe_notify_response_tx(Tx);
         _Other          -> ok
     end,
     notify_on_new_transactions(Rest).
@@ -31,7 +31,7 @@ notify_on_new_transactions([SignedTx | Rest]) ->
 %%% Internal functions
 %%%===================================================================
 
-notify_query_tx(Tx) ->
+maybe_notify_query_tx(Tx) ->
     case is_query_to_node_oracle(Tx) of
         true  -> aec_events:publish(oracle_query_tx_created, Tx);
         false -> ok
@@ -47,7 +47,7 @@ is_query_to_node_oracle(QueryTx) ->
     {ok, Keys} = aec_keys:pubkey(),
     Keys =:= aeo_query_tx:oracle(QueryTx).
 
-is_response_to_node_question(Signed) ->
+is_response_to_node_question(_Signed) ->
     %% TODO: Check if sender_address of interaction
     %% from a transaction is associated with node's pubkey
     %% We need to fetch interaction by id from state tree to check sender_address.
